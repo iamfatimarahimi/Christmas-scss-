c5c7e53cc092b7c0d468895e3eaef31c7b8ab046
Fatima Rahimi <iamfatimarahimi@gmail.com>
2025-02-20 11:12:08 +0330

    Adding media query

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+// const myCarouselElement = document.querySelector("#myCarousel");
+
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   touch: true,
+//   keyboard: false,
+// });
